75ce8199d8410bbf50bfa55ce038fa3b80ae4a18
nathanhleung <nathanhleung@users.noreply.github.com>
2025-04-20 22:38:19 -0700

    fix(examples/next): make dots lighter

--- examples/next/app/components/Background__prompt.tsx
+++ examples/next/app/components/Background__prompt.tsx
@@ -1,5 +1,5 @@
 Fixed positioned, inset 0, white background with grid-like dot pattern created by CSS radial-gradient, repeated.
 
-Dots should be light grey (#aaa) against a white background and not spaced too closely together, about 1rem apart.
+Dots should be light grey (#ddd) against a white background and not spaced too closely together, about 1rem apart.
 
 The background should fill the entire screen and should have a -1 z-index as to not cover the rest of the screen.
