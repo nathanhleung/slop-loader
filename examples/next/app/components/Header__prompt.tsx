7c1eb4851d882eee08a0b6b1599dd3b250273b9b
nathanhleung <nathanhleung@users.noreply.github.com>
2025-04-20 22:41:37 -0700

    fix(examples/next): remove !!START OVER

--- examples/next/app/components/Header__prompt.tsx
+++ examples/next/app/components/Header__prompt.tsx
@@ -1,6 +1,3 @@
-!!START OVER
-
-
 A React Header component that takes in a single `brand` prop. The brand displays in the upper-left in text-2xl on desktop, but which scales responsively according to screen size (xl on mobile). Tailwind classes are used to style as appropriate. The brand should link to "/" and be bold.
 
 The Header should have fixed positioning and have 8px margin from the top, left and right edges of the viewport. The Header should be translucent white, spanning the viewport width minus margin, 50% opacity with backdrop-blur and have 9999px border-radius. Height should be auto based on content. The Header itself should have 8px padding on the y-axis and 24px on the x-axis. DO NOT MIX UP TAILWIND UNITS AND PX UNITS. The text should be black. Add a 1px #aaa solid border. 
